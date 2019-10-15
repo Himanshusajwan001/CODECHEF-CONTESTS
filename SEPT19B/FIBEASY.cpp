c1518c90b549838f9ee44c39c0cd622e013f9316
himanshu sajwan <46423473+Himanshusajwan001@users.noreply.github.com>
2019-10-15 21:46:48 +0530

    Update FIBEASY.cpp

--- SEPT19B/FIBEASY.cpp
+++ SEPT19B/FIBEASY.cpp
@@ -12,10 +12,10 @@ int main() {
         if(n==1) cout << '0' << endl;
         else if(n>1 &&n<4) cout << '1' << endl;
         else {
-            if(n>=64) {
+            
                 while(n>=64)
                     n/=16;
-            }
+           
             if(n>=4 && n<8) cout << '2' << "\n";
             else if(n>=8 &&n<16) cout << '3' << "\n";
             else if(n>=16 &&n<32) cout << '0' << "\n";
